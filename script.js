8a2d82982fbbe3d068deaab11716e4e3cd14b4cd
MarfinCLX <glebtkacov5@gmail.com>
2026-09-11 17:24:30 +0100

    Added JS logic to create books and put books an Array

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
-function TheHobbit () {
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  if (!new.target) {
+  throw Error("You must use the 'new' operator to call the constructor"); 
+  }
+  this.id = crypto.randomUUID(),
+  this.title = title,
+  this.author = author,
+  this.pages = pages,
+  this.read = read;
+}
+
+function addBookToLibrary(title, author, pages, read) {
+  const newBook = new Book(title, author, pages, read);
+
+  myLibrary.push(newBook);
+}
+
+addBookToLibrary("The Hobbit", "JRR", 295, "Ready");
+
+console.log(myLibrary)
+
+
+/* function TheHobbit () {
      if (!new.target) {
     throw Error("You must use the 'new' operator to call the constructor");
   };
@@ -15,3 +39,5 @@ function TheHobbit () {
 const book = new TheHobbit();
 
 book.getInfo();
+
+*/
