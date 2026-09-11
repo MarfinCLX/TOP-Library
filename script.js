56539de4482a4237bbd2a6ad4a6b6668a7a1ac60
MarfinCLX <glebtkacov5@gmail.com>
2026-09-11 17:53:17 +0100

    Added JS logic to create a table with books by DOM

--- script.js
+++ script.js
@@ -18,9 +18,39 @@ function addBookToLibrary(title, author, pages, read) {
 }
 
 addBookToLibrary("The Hobbit", "JRR", 295, "Ready");
+addBookToLibrary("1984", "George Orwell", 328, "Ready");
 
-console.log(myLibrary)
+console.log(myLibrary);
 
+function filterLibrary () {
+  const div = document.querySelector('.table-container');
+  const table = document.querySelector('.table')
+
+  myLibrary.map(book => {
+    const tbody = document.createElement('tbody');
+
+    const row = document.createElement('tr');
+
+    const titleCell = document.createElement('td');
+    titleCell.textContent = book.title;
+
+    const authorCell = document.createElement('td');
+    authorCell.textContent = book.author;
+
+    const pagesCell = document.createElement('td');
+    pagesCell.textContent = book.pages;
+
+    const statusCell = document.createElement('td');
+    statusCell.textContent = book.status;
+
+    row.append(titleCell, authorCell, pagesCell, statusCell);
+    tbody.appendChild(row);
+    table.appendChild(tbody);
+    div.appendChild(table);
+  })
+}
+
+filterLibrary();
 
 /* function TheHobbit () {
      if (!new.target) {
